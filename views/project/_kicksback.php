73bae7df87f1ebd49e0c1fe3a10a577f438fa984
Victor Dekh <vic.dekh@gmail.com>
2022-11-08 12:53:33 +0300

    feat: add animation

--- views/project/_kicksback.php
+++ views/project/_kicksback.php
@@ -11,14 +11,14 @@ use yii\web\View;
         <div class="row">
             <div class="col-xl-10 offset-xl-1">
                 <div class="project-kicksback-1__title" style="background-image: url('<?= Url::to('@web/storage/project/kicksback/logo.svg?v=1') ?>')"></div>
-                <div class="project-kicksback-1__subtitle wow slideInLeft">
+                <div class="project-kicksback-1__subtitle wow fadeInDown">
                     <?= Yii::t('admin', 'Business') ?>
                 </div>
             </div>
         </div>
         <div class="row project-kicksback-1__description">
             <div class="col-xl-3 offset-xl-1 col-lg-4">
-                <div class="project-kicksback-1__text-1 wow slideInLeft">
+                <div class="project-kicksback-1__text-1 wow fadeInDown">
                     <?= Yii::t('app', 'Больше {br}никаких {br}посредников {br}и&nbsp;предоплаты {br}за&nbsp;рекламу', ['br' => '<br>']) ?>
                 </div>
             </div>
@@ -28,12 +28,12 @@ use yii\web\View;
                 </div>
             </div>
             <div class="col-xxl-3 col-lg-4">
-                <div class="project-kicksback-1__text-2 wow slideInRight">
+                <div class="project-kicksback-1__text-2 wow fadeInDown">
                     <?= Yii::t('app', 'На&nbsp;основе умных алгоритмов {firstline} угадывает желания клиентов и&nbsp;показывает заведения, товары и&nbsp;услуги, которые интересны именно&nbsp;иму', [
                         'firstline' => '<span>FIRSTLINE</span>',
                     ]) ?>
                 </div>
-                <div class="project-kicksback-1__text-3 wow slideInRight">
+                <div class="project-kicksback-1__text-3 wow fadeInDown">
                     <?= Yii::t('app', 'KICKSBACK дает возможность каждому бизнесу легально получить аналитические данные конкурентов, использовать собственные кассовые чеки для сохранения текущих клиентов и&nbsp;получения новых с&nbsp;уникальной платформой рекомендаций FIRSTLINE') ?>
                 </div>
             </div>
@@ -45,10 +45,10 @@ use yii\web\View;
     <div class="container">
         <div class="row">
             <div class="col-xl-4 offset-xl-4 col-md-6 offset-md-3 col-sm-8 offset-sm-2">
-                <div class="project-kicksback-2__title">
+                <div class="project-kicksback-2__title wow fadeInDown">
                     <?= Yii::t('app', 'Что такое кикcбэк?') ?>
                 </div>
-                <div class="project-kicksback-2__text">
+                <div class="project-kicksback-2__text wow fadeInDown">
                     <?= Yii::t('app', 'Это бонус, который выплачивается клиенту за&nbsp;приведенного друга. Вы&nbsp;создаете в&nbsp;нашем приложении программу лояльности, и&nbsp;число ваших постоянных клиентов начинает быстро расти.') ?>
                 </div>
             </div>
@@ -60,19 +60,19 @@ use yii\web\View;
     <div class="container">
         <div class="row">
             <div class="col-xl-8 offset-xl-2 col-lg-10 offset-lg-1">
-                <div class="project-kicksback-3__subtitle wow fadeIn">
+                <div class="project-kicksback-3__subtitle wow fadeInDown">
                     <?= Yii::t('app', 'Успешный бизнес {br}вместе с&nbsp;приложением', ['br' => '<br>']) ?>
                 </div>
-                <div class="project-kicksback-3__title" style="background-image: url('<?= Url::to('@web/storage/project/kicksback/image-3.svg?v=1') ?>')"></div>
+                <div class="project-kicksback-3__title wow fadeInDown" style="background-image: url('<?= Url::to('@web/storage/project/kicksback/image-3.svg?v=1') ?>')"></div>
                 <div class="project-kicksback-3__info">
-                    <div class="project-kicksback-3__links">
+                    <div class="project-kicksback-3__links wow fadeInDown">
                         <?= Html::a('', 'https://kicksback.app/', ['class' => 'project-kicksback-3__link project-kicksback-3__link_site', 'target' => '_blank']) ?>
                         <div class="project-kicksback-3__hr"></div>
                         <?= Html::a('', 'https://play.google.com/store/apps/details?id=com.sessia.kickbacks', ['class' => 'project-kicksback-3__link project-kicksback-3__link_google', 'target' => '_blank']) ?>
                         <div class="project-kicksback-3__hr"></div>
                         <?= Html::a('', 'https://apps.apple.com/app/kicksback-cashback-and-gifts/id1546115424', ['class' => 'project-kicksback-3__link project-kicksback-3__link_apple', 'target' => '_blank']) ?>
                     </div>
-                    <div class="project-kicksback-3__text">
+                    <div class="project-kicksback-3__text wow fadeInDown">
                         <?= Yii::t('app', 'Бесплатное приложение для&nbsp;привлечения новых клиентов и&nbsp;увеличения лояльной аудитории') ?>
                     </div>
                 </div>
