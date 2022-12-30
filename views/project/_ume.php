00fea2906d0b600931c775feba819fed9b76c10e
sallamyemen <sallamyemen@hotmail.com>
2022-12-30 15:16:42 +0300

    ume project creation

---
+++ views/project/_ume.php
@@ -0,0 +1,138 @@
+<?php
+
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\web\View;
+
+?>
+
+<section class="project-ume-1" style="background-image: url('<?= Url::to('@web/storage/project/ume/bg-1.svg?v=1') ?>')">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-10 offset-lg-1 info-parent">
+                <div class="project-ume-1__image wow fadeInDown" style="background-image: url('<?= Url::to('@web/storage/project/ume/image-1-3.png?v=1') ?>')"></div>
+                <div class="project-ume-1__image wow fadeInDown dog-hand" style="background-image: url('<?= Url::to('@web/storage/project/ume/image-1-2.png?v=1') ?>')"></div>
+            </div>
+
+        </div>
+    </div>
+</section>
+
+<section class="project-ume-2">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-10 offset-lg-1">
+                <div class="project-ume-2__image wow fadeInDown" style="background-image: url('<?= Url::to('@web/storage/project/ume/image-2-1.png?v=1') ?>')"></div>
+            </div>
+            <div class="info-flex">
+                <div class="col-lg-6 info wow fadeInDown">
+                    <div class="info-img"><img src="<?= Url::to('@web/storage/project/ume/image-2-2.png?v=1') ?>" alt=""></div>
+                </div>
+                <div class="col-lg-6"><p class="project-ume-2__text">Приложение, благодаря которому владелец и питомец смогут лучше понимать друг друга, с функционалом онлайн-магазина, элементами соцсети, интегрированной программой лояльности и картой услуг для питомцев</p></div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="project-ume-3" style="background-image: url('<?= Url::to('@web/storage/project/ume/bg-3-1.svg?v=1') ?>'),
+                                                        url('<?= Url::to('@web/storage/project/ume/bg-3-2.svg?v=1') ?>')   ">
+    <div class="container">
+        <div class="row project-ume-3__row-img">
+            <div class="project-ume-3__image wow fadeInDown">
+                <img src="<?= Url::to('@web/storage/project/ume/image-3-1.png?v=1') ?>" alt="">
+            </div>
+            <div class="project-ume-3__flex wow fadeInDown">
+                <p class="project-ume-3__text-big project-ume-3__left">
+                    UME — <br>это премиальные корма с добавлением гипоаллергенного белка насекомых и корня шестилетнего белого императорского женьшеня</p>
+                <p class="project-ume-3__text-small project-ume-3__right">Проект стартует в РФ с 2022 г.
+                    До 2025 года запланировал выход на рынки 10 стран, включая государства ЕС и СНГ, Вьетнам и Сингапур</p>
+            </div>
+            <!--div class="row project-ume-3__pos-back" style="background-image: url('<?//= Url::to('@web/storage/project/ume/bg-3-2.svg?v=1') ?>')"></div-->
+        </div>
+    </div>
+</section>
+
+<section class="project-ume-4" style="background-image: url('<?= Url::to('@web/storage/project/ume/bg-4.svg?v=1') ?>') ">
+    <div class="container">
+        <div class="row project-ume-4__row-1 wow fadeInUp">
+            <div class="project-ume-4__text-left">
+                <p class="project-ume-4__text-title">Соцсеть</p>
+                <p class="project-ume-4__text-info">приглашайте друзей своего питомца и ведите ленту с его постами</p>
+            </div>
+            <div class="project-ume-4__phone-1">
+                <img src="<?= Url::to('@web/storage/project/ume/image-4-1.png?v=1') ?>" alt="">
+            </div>
+            <div class="project-ume-4__text-right">
+                <p class="project-ume-4__text-title">Маркетплейс</p>
+                <p class="project-ume-4__text-info">покупайте с выгодой премиальные продукты для питомцев</p>
+            </div>
+            <div class="project-ume-4__1st-flex"></div>
+        </div>
+        <div class="row project-ume-4__row-2 wow fadeInUp">
+            <div class="project-ume-4__phone-2">
+                <img src="<?= Url::to('@web/storage/project/ume/image-4-3.png?v=1') ?>" alt="">
+            </div>
+            <div class="project-ume-4__info-right">
+                <div class="project-ume-4__image-right">
+                    <img src="<?= Url::to('@web/storage/project/ume/image-4-2.png?v=1') ?>" alt="">
+                </div>
+                <p class="project-ume-4__text-title">Программа лояльности</p>
+                <p class="project-ume-4__text-info">получайте бонусы за покупки и подписки, накапливайте баллы  UME, повышайте статус и получайте дополнительные  услуги для питомца</p>
+            </div>
+        </div>
+        <div class="row project-ume-4__row-3 wow fadeInUp">
+            <div class="project-ume-4__phone-3">
+                <img src="<?= Url::to('@web/storage/project/ume/image-4-4.png?v=1') ?>" alt="">
+            </div>
+            <div class="project-ume-4__info-left">
+                <p class="project-ume-4__text-title">Консьерж</p>
+                <p class="project-ume-4__text-info">быстро находите профессиональные зооуслуги и сервисы</p>
+            </div>
+        </div>
+        <div class="row project-ume-4__row-4 wow fadeInUp">
+            <div class="project-ume-4__phone-4">
+                <img src="<?= Url::to('@web/storage/project/ume/image-4-5.png?v=1') ?>" alt="">
+            </div>
+            <div class="project-ume-4__info-right-2">
+                <p class="project-ume-4__text-title">Документы</p>
+                <p class="project-ume-4__text-info">храните все документы в одном месте и держите их всегда под рукой</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="project-ume-5" style="background-image: url('<?= Url::to('@web/storage/project/ume/bg-5.svg?v=1') ?>') ">
+    <div class="container">
+        <div class="project-ume-5__rows">
+            <p class="project-ume-5__text-info wow fadeInDown">Приложение UME помогает лучше<br>понимать своих четвероногих друзей<br>и подбирать для них всё самое лучшее</p>
+            <p class="project-ume-5__text-big wow fadeInDown">Ведь они<br>совсем как мы</p>
+            <div class="project-ume-5__stores wow fadeInDown">
+                <div class="project-ume-5__android">
+                    <a href="#!"><img src="<?= Url::to('@web/storage/project/icon-google.svg?v=1') ?>" alt="android"></a>
+                </div>
+                <div class="project-ume-5__apple">
+                    <a href="#!"><img src="<?= Url::to('@web/storage/project/icon-apple.svg?v=1') ?>" alt="apple"></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+
+
+
+
+
+
+
+
+<?php
+
+$js = <<<JS
+
+new WOW().init();
+
+JS;
+$this->registerJs($js, View::POS_END);
